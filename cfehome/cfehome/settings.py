8dc3bcba3ab6ca10eaf800440c6152d5ea1d9cd3
Yusuf Demir <61087995+Ayayaw@users.noreply.github.com>
2025-04-14 10:41:52 +0300

    Add database url

--- cfehome/cfehome/settings.py
+++ cfehome/cfehome/settings.py
@@ -90,6 +90,17 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASE_URL = config("DATABASE_URL", cast=str, default="")
+if DATABASE_URL:
+    import dj_database_url
+    if DATABASE_URL.startswith("postgres://") or DATABASE_URL.startswith("postgresql://"):
+        DATABASES = {
+            'default': dj_database_url.config(
+                default=DATABASE_URL,
+                )
+        }
+
+
 
 
 # Password validation
